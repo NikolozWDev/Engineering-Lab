12e05a5aa3d080bff3a484ead34fed94215595aa
Nikoloz Gigiashvili <gigiashvilinikoloz@gmail.com>
2026-07-25 00:35:17 +0400

    main formula of Pascal's Triangle

--- codeTests.py
+++ codeTests.py
@@ -1,4 +1,16 @@
 import codewars_test as test
+from solution import pascals_triangle
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(pascals_triangle(1), [1],"1 level triangle incorrect");
+        test.assert_equals(pascals_triangle(2), [1,1,1],"2 level triangle incorrect");
+        test.assert_equals(pascals_triangle(3), [1,1,1,1,2,1],"3 level triangle incorrect");
+main formula: n! / k!(n - k)!
+
+import codewars_test as test
 from solution import solve
 
 @test.describe("Fixed Tests")
